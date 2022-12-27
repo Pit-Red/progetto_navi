b9844ac1780cc9586777fb1bb0df80b1b659072f
AleScavo <80278389+AleScavo@users.noreply.github.com>
2022-12-27 13:38:23 +0000

    Add files via upload

--- down.sh
+++ down.sh
@@ -1,7 +1,42 @@
-   #da finire
-    for arg in "$@";do
-        #ifper controllo se nome file da scaricare è valido
-        curl -LJO https://raw.githubusercontent.com/Pit-Red/progetto_navi/main/$i;
+#!/bin/bash
 
-    i=$((i + 1));
-done
+if [ ! -d ".old" ]
+then 
+	echo -e "\ncreata cartella .old\n"
+	mkdir .old
+fi
+
+
+case $1 in
+
+	main)
+	echo -e "\nDownload di main.c in corso...\n"
+	mv ./main.c ./.old/main-old.c
+	curl -LJO https://raw.githubusercontent.com/Pit-Red/progetto_navi/main/main.c
+	;;
+
+	nave)
+	echo -e "\nDownload di nave.c in corso\n"
+	mv ./nave.c ./.old/nave-old.c
+	curl -LJO https://raw.githubusercontent.com/Pit-Red/progetto_navi/main/nave.c
+	;;
+
+	porto)
+	echo -e "\nDownload di porto.c in corso\n"
+	mv ./porto.c ./.old/porto-old.c
+	curl -LJO https://raw.githubusercontent.com/Pit-Red/progetto_navi/main/porto.c
+	;;
+
+	utilities)
+	mv ./utilities.c ./.old/utilities-old.c
+	mv ./utilities.h ./.old/utilities-old.h
+	echo -e "\nDownload di utilites.c e utilities.h in corso\n"
+	curl -LJO https://raw.githubusercontent.com/Pit-Red/progetto_navi/main/utilites.c
+	curl -LJO https://raw.githubusercontent.com/Pit-Red/progetto_navi/main/utilites.h
+	;;
+
+	*)
+	echo -e "\nFile non esistente o scritto incorrettamente\n"
+	;;
+
+esac
